0b0b021a0abdc52c5cbfb9f30b54798ebff9cd8e
JoaoPaulo-creator <joaosarto@gmail.com>
2025-02-01 00:25:41 -0300

    refactor: adicionado log para verificar em que horário foi feita uma verificação

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"horario-simples/schedules"
+	"log"
 	"time"
 )
 
@@ -10,9 +11,11 @@ var p = fmt.Println
 
 func main() {
 	ticker := time.NewTicker(time.Hour * 2) // checa todos os dias
+	now := time.Now().Local()
 	defer ticker.Stop()
 
 	for t := range ticker.C {
+		log.Printf("Horário de checagem: %v\n", now)
 		if t.Hour() == 15 && t.Minute() == 05 {
 			p(schedules.ScheduleForTheDay())
 		}
